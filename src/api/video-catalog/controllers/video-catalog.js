290cf2dab2b8b455c927e49f5f51bdc18ccc6f10
Kireenkov22rus) <d.kireenkov32rus@yandex.ru>
2024-09-03 14:13:07 +0300

    new11

---
+++ src/api/video-catalog/controllers/video-catalog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-catalog controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::video-catalog.video-catalog');
---
+++ src/api/video-catalog/routes/video-catalog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * video-catalog router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::video-catalog.video-catalog');
