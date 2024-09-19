43d00aa2bd29b17af41af44e66c0d5756796976f
Kireenkov22rus) <d.kireenkov32rus@yandex.ru>
2024-09-19 19:21:55 +0300

    first commit12354541363689175805777911

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,8 @@
 module.exports = [
+  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  // {
-  //   name: 'strapi::cors',
-  //   config: {
-  //     enabled: true,
-  //     origin: ['http://dkireeeo.beget.tech', '*'], // Твой фронтенд-домен
-  //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  //     headers: ['Content-Type', 'Authorization'],
-  //   },
-  // },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
